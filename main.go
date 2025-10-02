98886745108e57af4b157e64e168700b202cfac7
Ajasf444 <longstephen444@gmail.com>
2025-10-02 01:35:45 -0500

    added lowercase to cleanInput()

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -9,5 +10,6 @@ func main() {
 }
 
 func cleanInput(text string) []string {
-	return []string{}
+	lowerText := strings.ToLower(text)
+	return strings.Fields(lowerText)
 }
