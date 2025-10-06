4a101d2bac81a5a039d90fc188b6047eeb5b2e01
Ajasf444 <longstephen444@gmail.com>
2025-10-06 02:05:45 -0500

    added supportedCommands map

--- main.go
+++ main.go
@@ -12,11 +12,41 @@ func main() {
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		fmt.Println(scanner.Text())
+		cleanedInput := cleanInput(scanner.Text())
 	}
 }
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
 	return strings.Fields(lowerText)
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("TODO:Help content goes here")
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var supportedCommands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
 }
