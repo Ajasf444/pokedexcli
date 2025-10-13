60f4b5bba4c5a41d09fbc2c25964e0292f3c7f55
Ajasf444 <longstephen444@gmail.com>
2025-10-12 23:56:17 -0500

    moved command functions and structs into repl.go

--- repl.go
+++ repl.go
@@ -1,4 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
 
 func startRepl() {
 	// TODO: import functionality from main.go
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Usage:")
+	fmt.Print("\n")
+	for _, commandInfo := range commandRegistry {
+		fmt.Printf("%v: %v\n", commandInfo.Name, commandInfo.Description)
+	}
+	return nil
+}
+
+func commandMap() error {
+	// TODO: refactor into repl.go
+	return nil
+}
+
+type cliCommand struct {
+	Name        string
+	Description string
+	Callback    func() error
+}
+
+func registerCommand(name, description string, callback func() error) {
+	commandRegistry[name] = &cliCommand{
+		Name:        name,
+		Description: description,
+		Callback:    callback,
+	}
 }
