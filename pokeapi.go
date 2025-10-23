a92c146e5d189f96264890f2523c11023304e5e8
Ajasf444 <longstephen444@gmail.com>
2025-10-23 04:41:19 -0500

    added net/http import to pokeapi.go

--- pokeapi.go
+++ pokeapi.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"net/http"
+)
+
 type Response struct {
 	Count    int    `json:"count"`
 	Next     string `json:"next"`
@@ -12,4 +16,5 @@ type Response struct {
 
 func getRequest() {
 	// TODO: use PokeAPI to make get request
+	http.Get("blah")
 }
