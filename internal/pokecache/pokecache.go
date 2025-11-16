4f9b83860e192e059f21898e319ca627d8f61fbf
Ajasf444 <longstephen444@gmail.com>
2025-11-16 00:10:38 -0600

    created pokecache

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,25 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	mu      sync.Mutex
+	entries map[string]cacheEntry
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
