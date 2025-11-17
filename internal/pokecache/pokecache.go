0e8ec52fad771e1699dce29d58ebbc7917131bd3
Ajasf444 <longstephen444@gmail.com>
2025-11-17 02:15:30 -0600

    added some Cache logic

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,6 +1,7 @@
 package pokecache
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
@@ -15,11 +16,40 @@ type cacheEntry struct {
 	val       []byte
 }
 
+func NewCache(interval time.Duration) *Cache {
+	newCache := Cache{}
+	go newCache.reapLoop(interval)
+	return &newCache
+}
+
 func (c *Cache) Add(key string, val []byte) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.entries[key] = cacheEntry{
 		createdAt: time.Now(),
 		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) (val []byte, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, ok := c.entries[key]
+	if !ok {
+		return []byte{}, ok
+	}
+	return entry.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	ticker := time.NewTicker(interval)
+	t := <-ticker.C
+	for key, entry := range c.entries {
+		if t.Sub(entry.createdAt) > interval {
+			fmt.Printf("Deleting %v from cache.", key)
+			delete(c.entries, key)
+		}
 	}
 }
