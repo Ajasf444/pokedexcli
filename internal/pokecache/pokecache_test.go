1aadd3ce81b3f2d455f7145b11e32638a23a292d
Ajasf444 <longstephen444@gmail.com>
2025-11-23 00:49:07 -0600

    added cases for TestAddGet()

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -2,8 +2,25 @@ package pokecache
 
 import (
 	"testing"
+	"time"
 )
 
-func TestCache(t *testing.T) {
-	// TODO: add tests
+func TestAddGet(t *testing.T) {
+	const interval = 5 * time.Second
+	cases := []struct {
+		input    string
+		expected []byte
+	}{
+		{
+			input:    "www.website.com",
+			expected: []byte("data_1"),
+		},
+		{
+			input:    "www.website2.com",
+			expected: []byte("data_2"),
+		},
+	}
+	for in, out := range cases {
+		// TODO: implement testing
+	}
 }
