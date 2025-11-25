2025e37a5a8d1d3f1a209e4d0b22776f7547acb4
Ajasf444 <longstephen444@gmail.com>
2025-11-25 01:26:19 -0600

    added a TestReapLoop() function

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -9,21 +9,50 @@ import (
 func TestAddGet(t *testing.T) {
 	const interval = 5 * time.Second
 	cases := []struct {
-		input    string
-		expected []byte
+		key   string
+		value []byte
 	}{
 		{
-			input:    "www.website.com",
-			expected: []byte("data_1"),
+			key:   "www.website.com",
+			value: []byte("data_1"),
 		},
 		{
-			input:    "www.website2.com",
-			expected: []byte("data_2"),
+			key:   "www.website2.com",
+			value: []byte("data_2"),
 		},
 	}
-	for in, out := range cases {
-		t.Run(fmt.Sprintf("Test Case %v", in), func(t *testing.T) {
-			// TODO: add tests
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("Test Case %v", i), func(t *testing.T) {
+			cache := NewCache(interval)
+			cache.Add(c.key, c.value)
+			val, ok := cache.Get(c.key)
+			if !ok {
+				t.Errorf("expected to find key")
+				return
+			}
+			if string(val) != string(c.value) {
+				t.Errorf("expected to find value")
+				return
+			}
 		})
+	}
+}
+
+func TestReapLoop(t *testing.T) {
+	const baseTime = 5 * time.Millisecond
+	const waitTime = baseTime + 5*time.Millisecond
+	const website = "www.example.com"
+	cache := NewCache(baseTime)
+	cache.Add(website, []byte("data"))
+	_, ok := cache.Get(website)
+	if !ok {
+		t.Errorf("expected to find key")
+		return
+	}
+	time.Sleep(waitTime)
+	_, ok = cache.Get(website)
+	if ok {
+		t.Errorf("expected to not find key")
+		return
 	}
 }
