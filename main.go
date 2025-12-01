f7df407ed0d4917559033ccdf3f92e4633e94a9c
Ajasf444 <longstephen444@gmail.com>
2025-12-01 10:13:27 -0600

    added config generation to main()

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"time"
+
+	"github.com/Ajasf444/pokedexcli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	cfg := &Config{
+		client:     pokeapi.NewClient(5*time.Minute, 5*time.Second),
+		pagination: &pokeapi.Pagination{Next: "https://pokeapi.co/api/v2/location-area"},
+	}
+	startRepl(cfg)
 }
