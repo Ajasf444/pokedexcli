a0ca8b52d1773a0ce510486513699c81257d17d2
Ajasf444 <longstephen444@gmail.com>
2025-12-08 00:52:25 -0600

    added GetLocationContent() outline

---
+++ internal/pokeapi/get_pokemon.go
@@ -0,0 +1,44 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+)
+
+// TODO: update this logic
+func (c *Client) GetLocationContent(pageURL *string) (LocationAreaResponse, error) {
+	url := baseURL + "/location-area"
+	if pageURL != nil {
+		url = *pageURL
+	}
+	if data, ok := c.cache.Get(url); ok {
+		response := LocationAreaResponse{}
+		if err := json.Unmarshal(data, &response); err != nil {
+			return LocationAreaResponse{}, err
+		}
+		return response, nil
+	}
+	resp, err := c.httpClient.Get(url)
+	if err != nil {
+		return LocationAreaResponse{}, errors.New("error: failed to get response")
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationAreaResponse{}, errors.New("error: failed to read response body")
+	}
+	response := LocationAreaResponse{}
+	if err := json.Unmarshal(body, &response); err != nil {
+		return LocationAreaResponse{}, errors.New("error: unable to unmarshal LocationAreaResponse")
+	}
+	return response, nil
+}
+
+func PrintPokemon(resp LocationAreaResponse) {
+	data := resp.Results
+	for _, location := range data {
+		fmt.Println(location.Name)
+	}
+}
