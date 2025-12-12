c4cf2ed364aacac0ad3519c01134fffa3a61b23f
Ajasf444 <longstephen444@gmail.com>
2025-12-12 01:38:14 -0600

    moved REPL commands into another file

--- repl.go
+++ repl.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -59,67 +58,4 @@ func startRepl(cfg *Config) {
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
 	return strings.Fields(lowerText)
-}
-
-func commandExit(cfg *Config, arg string) error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp(cfg *Config, arg string) error {
-	fmt.Println("Usage:")
-	fmt.Print("--------------------------------------\n")
-	for _, commandInfo := range commandRegistry {
-		fmt.Printf("%v: %v\n", commandInfo.Name, commandInfo.Description)
-	}
-	return nil
-}
-
-func commandMap(cfg *Config, arg string) error {
-	url := cfg.pagination.Next
-	results, err := cfg.client.GetLocations(url)
-	if err != nil {
-		return err
-	}
-	pokeapi.UpdatePagination(cfg.pagination, results)
-	pokeapi.PrintLocationArea(results)
-	return nil
-}
-
-func commandMapB(cfg *Config, arg string) error {
-	url := cfg.pagination.Back
-	if url == nil {
-		return errors.New("You are on the first page!")
-	}
-	results, err := cfg.client.GetLocations(url)
-	if err != nil {
-		return err
-	}
-	pokeapi.UpdatePagination(cfg.pagination, results)
-	pokeapi.PrintLocationArea(results)
-	return nil
-}
-
-func commandExplore(cfg *Config, location string) error {
-	results, err := cfg.client.GetLocationContent(location)
-	if err != nil {
-		return err
-	}
-	pokeapi.PrintPokemon(results)
-	return nil
-}
-
-type cliCommand struct {
-	Name        string
-	Description string
-	Callback    func(*Config, string) error
-}
-
-func registerCommand(name, description string, callback func(*Config, string) error) {
-	commandRegistry[name] = &cliCommand{
-		Name:        name,
-		Description: description,
-		Callback:    callback,
-	}
 }
