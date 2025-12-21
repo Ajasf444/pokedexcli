a2835a82624196fd7d726b0cb89a847323507174
Stephen Long <longstephen444@gmail.com>
2025-12-21 00:04:45 -0600

    added outer logic to CatchPokemon()

--- internal/pokeapi/catch_pokemon.go
+++ internal/pokeapi/catch_pokemon.go
@@ -1,5 +1,18 @@
 package pokeapi
 
+import (
+	"encoding/json"
+)
+
 func (c *Client) CatchPokemon(name string) (Pokemon, error) {
-	return Pokemon{}, nil
+	url := baseURL + "/ability/" + name
+	data, ok := c.cache.Get(url)
+	if !ok {
+		//TODO: client work
+	}
+	pokemon := Pokemon{}
+	if err := json.Unmarshal(data, &pokemon); err != nil {
+		return Pokemon{}, nil
+	}
+	return pokemon, nil
 }
