ced997ccdd0b2679431fbed9940a387f258ecd80
Ajasf444 <longstephen444@gmail.com>
2026-01-09 03:01:37 -0600

    added some elementary logic to InspectPokemon()

--- internal/pokeapi/inspect_pokemon.go
+++ internal/pokeapi/inspect_pokemon.go
@@ -1,5 +1,12 @@
 package pokeapi
 
+import "fmt"
+
 func (c *Client) InspectPokemon(name string) error {
+	pokemon, ok := c.caughtPokemon[name]
+	if !ok {
+		fmt.Printf("%v has not been caught!", name)
+	}
+	fmt.Println(pokemon) // TODO: handle pokemon data
 	return nil
 }
