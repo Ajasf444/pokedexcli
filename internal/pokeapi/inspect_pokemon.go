cbb422c1eb298e372d06385f61a979d280b2fbfc
Ajasf444 <longstephen444@gmail.com>
2026-01-10 00:02:02 -0600

    replaced print with return error from InspectPokemon()

--- internal/pokeapi/inspect_pokemon.go
+++ internal/pokeapi/inspect_pokemon.go
@@ -1,11 +1,14 @@
 package pokeapi
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func (c *Client) InspectPokemon(name string) error {
 	pokemon, ok := c.caughtPokemon[name]
 	if !ok {
-		fmt.Printf("%v has not been caught!", name)
+		return errors.New(fmt.Sprintf("%v has not been caught!", name))
 	}
 	fmt.Println(pokemon) // TODO: handle pokemon data
 	return nil
