709500017f1d43fe58da8dca5a277d7aa589be5f
Ajasf444 <longstephen444@gmail.com>
2026-01-20 01:36:23 -0600

    added printing logic to Pokedex()

--- internal/pokeapi/pokedex.go
+++ internal/pokeapi/pokedex.go
@@ -1,5 +1,11 @@
 package pokeapi
 
+import "fmt"
+
 func (c *Client) Pokedex() error {
+	fmt.Println("Your Pokedex:")
+	for _, pokemon := range c.caughtPokemon {
+		fmt.Printf("  - %v\n", pokemon.Name)
+	}
 	return nil
 }
