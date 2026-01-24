f7eafb9184e8d22713c7e76c3a849d8b53840e57
Ajasf444 <longstephen444@gmail.com>
2026-01-24 07:21:06 -0600

    removed a TODO

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/Ajasf444/pokedexcli/internal/pokeapi"
 )
 
-// TODO: move baseURL to another file
 func main() {
 	cfg := &Config{
 		client:     pokeapi.NewClient(5*time.Minute, 5*time.Second),
